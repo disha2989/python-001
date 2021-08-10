81676b8203960d25c46cd83828acdf720722adab
disha2989 <71320045+disha2989@users.noreply.github.com>
2021-08-10 14:37:36 +0530

    done

--- Activity_03.py
+++ Activity_03.py
@@ -1,9 +1,9 @@
 x= input('enter first string')
 y= input('enter second string')
-z='\t'
+
 w='\n'
 print('first output')
-print(x + z +y)
+print(x+y)
 
 print('second output')
-print(((x+z+y)+w)*5)
+print(((x+y)+w)*5)
